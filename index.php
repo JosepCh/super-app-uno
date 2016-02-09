b95823229438e10a32d86c1060a164c68640b485
Josep Chanzá <josepchanza@MacBook-Pro-de-Josep.local>
2016-02-09 18:44:59 +0100

    Instancias

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head> 
+<body>
+
+<h1>Icono del Dia</h1>
+$instancia=$_SERVER["WEBSITE_INSTANCE_ID"];
+echo($instancia);
+
+<img src="randomicon.php"/>
+
+</body>
+</html>
+
+
+
